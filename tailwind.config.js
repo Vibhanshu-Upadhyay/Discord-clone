d42c98d733e0a96192ffd1db4bddfca016c8d0c5
Vibhanshu <38998248+Vibhanshu-Upadhyay@users.noreply.github.com>
2024-01-28 14:09:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      abc: ['"Abcgintonord 800"', "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
